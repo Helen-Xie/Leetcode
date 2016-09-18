83b6baf901494183fdc31105e5e84ccd159c100a
Yulin <xylin821@163.com>
2016-09-18 19:26:53 +0800

    7th

---
+++ Merge Sorted Array.go
@@ -0,0 +1,53 @@
+func merge(nums1 []int, m int, nums2 []int, n int)  {
+    var f1, f2 int
+    
+    if nums1[0]<=nums1[m-1]{
+        f1=0
+    }else{
+        f1=1
+    }
+    
+    if nums2[0]<=nums2[n-1]{
+        f2=0
+    }else{
+        f2=1
+    }
+    
+    if f1!= f2 {
+        for i:=0; i<=n/2; i++ {
+            nums2[i] = nums2[n-1-i]
+        }
+    }
+    
+    pos1 := m-1
+    pos2 := n-1
+    pos := m+n-1
+    
+    if f1 != 0 {
+        for pos1>=0 && pos2 >=0 {
+            if nums1[pos1]<nums2[pos2] {
+                nums1[pos] = nums1[pos1]
+                pos1--
+                pos--
+            } else {
+                nums1[pos] = nums2[pos2]
+                pos2--
+                pos--
+            }
+        }
+    } else {
+        for pos1>=0 && pos2 >=0 {
+            if nums1[pos1]>nums2[pos2] {
+                nums1[pos] = nums1[pos1]
+                pos1--
+                pos--
+            } else {
+                nums1[pos] = nums2[pos2]
+                pos2--
+                pos--
+            }
+        } 
+    }
+    
+    return
+}

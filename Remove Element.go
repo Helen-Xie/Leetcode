3d141016105879bd239ae1e2cb004e2221508e3e
Yulin <xylin821@163.com>
2016-09-18 11:57:00 +0800

    5th

---
+++ Remove Element.go
@@ -0,0 +1,12 @@
+func removeElement(nums []int, val int) int {
+    var pos = 0 
+    
+    for i := 0; i < len(nums); i++{
+        if nums[i] != val {
+            nums[pos] = nums[i]
+            pos++
+        }
+    }
+    
+    return pos
+}
